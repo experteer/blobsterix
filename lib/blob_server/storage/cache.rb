245175191c02ad37faac9a8dca0335981429732f
dsudmann <suddani@googlemail.com>
2014-01-29 15:34:59 +0100

    refactoring

--- lib/blob_server/storage/cache.rb
+++ lib/blob_server/storage/cache.rb
@@ -1,28 +1,35 @@
 module BlobServer
 	module Storage
 		class Cache
+
 			def initialize(path="../cache")
 				@path = path
 			end
+
 			def path_prepare(key)
 				p = path(key)
 				FileUtils.mkdir_p(File.dirname(p))
 				p
 			end
+
 			def path(key=nil)
 				key ? File.join(@path, Murmur.map_filename(key)) : @path
 			end
+
 			def get(key)
 				FileSystemMetaData.new(path(key))
 			end
+
 			def put(key, data)
 				FileSystemMetaData.new(path(key)).write() {|f|
 					f.write(data)
 				}
 			end
+
 			def delete(key)
 				FileSystemMetaData.new(path(key)).delete if exists?(key)
 			end
+
 			def exists?(key)
 				File.exist?(path(key))
 			end
