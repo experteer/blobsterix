ff7e91f2e3f221b471cbeecb174bca8955475910
dsudmann <suddani@googlemail.com>
2014-03-03 18:27:53 +0100

    removed unneeded methods from blob_access

--- lib/blobsterix/helper/blob_access.rb
+++ lib/blobsterix/helper/blob_access.rb
@@ -12,16 +12,8 @@ module Blobsterix
        @identifier||= "#{bucket}_#{id.gsub("/","_")}_#{trafo.map {|trafo_pair|"#{trafo_pair[0]}_#{trafo_pair[1]}"}.join(",")}.#{subtype}"
     end
 
-    def base_identifier
-       @identifier||= "#{bucket}_#{id.gsub("/","_")}_"
-    end
-
     def to_s()
       "BlobAccess: bucket(#{bucket}), id(#{id}), trafo(#{trafo}), accept_type(#{accept_type})"
-    end
-
-    def is_specific?
-      trafo.size > 0 || (subtype && subtype.length > 0)
     end
 
     private
