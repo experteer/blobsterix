bd748cc6a678a59b44ba4cba35d12bbd7a0ec31c
Suddani <suddani@googlemail.com>
2014-03-05 00:25:31 +0100

    updated project templates to give a better starting point

--- templates/app/config.rb
+++ templates/app/config.rb
@@ -17,33 +17,44 @@ module Blobsterix
   # end
 
   # Override those methods if you want to change the storage and cache dirs from their default location
-  # def self.storage_dir(logger)
+  # For a different way check the environments files
+  # def self.storage_dir
   #   Blobsterix.root.join("contents")
   # end
-  # def self.cache_dir(logger)
+  # def self.cache_dir
   #   Blobsterix.root.join("cache")
   # end
 
   # Override those if you want to change the default storage and cache system.
-  # def self.storage(logger)
+  # For a different way check the environments files
+  # def self.storage
   #   @@storage ||= Storage::FileSystem.new(Blobsterix.storage_dir)
   # end
-  # def self.cache(logger)
+  # def self.cache
   #   @@cache ||= Storage::Cache.new(Blobsterix.cache_dir)
   # end
 
 
   # Override this method if you want to use a different transformation manager.
+  # For a different way check the environments files
   # normally not needed
-  # def self.transformation(logger)
+  # def self.transformation
   #   @@transformation ||= Blobsterix::Transformations::TransformationManager.new
   # end
 
   # Override this method incase you expect the trafo string in a special format.
+  # For a different way check the environments files
   # The return should be in format of: trafoName_value,trafoName_value,....
   # Example: scale_2,rotate_20
   # def self.decrypt_trafo(trafo_string,logger)
   #   trafo_string
+  # end
+
+  # Use a specific storage and cache event listener. Check the environment files for a better description
+  # def self.storage_event_listener
+  #   @storage_event_listener||=lambda{|target, blob_access|
+  #     logger.info("#{target}: #{blob_access}")
+  #   }
   # end
 end
 
