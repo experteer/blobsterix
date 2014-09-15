d2108591788df9c4574d35be2c5215b5daac938f
dsudmann <suddani@googlemail.com>
2014-09-15 12:03:06 +0200

    removed comment

--- lib/blobsterix/service.rb
+++ lib/blobsterix/service.rb
@@ -1,23 +1,9 @@
 module Blobsterix
   class Service < Goliath::API
     use Goliath::Rack::Params
-    # use SudiMiddleWare
+
     include Logable
-=begin
-    def on_headers(env, headers)
-      env.logger.info 'received headers: ' + headers.inspect
-        env['async-headers'] = headers
-    end
 
-    def on_body(env, data)
-      env.logger.info 'received data: ' + data
-      (env['async-body'] ||= '') << data
-    end
-
-    def on_close(env)
-      env.logger.info 'closing connection'
-    end
-=end
     def response(env)
       # env["params"] = params
       call_stack(env, BlobApi, StatusApi, S3Api)
